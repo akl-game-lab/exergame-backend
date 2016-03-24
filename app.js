2306dd36bd5d444dd72ae34c1f0f6f6f69862351
Saren Currie <saren@sarencurrie.com>
2016-03-24 14:25:39 +1300

    Working user login and registration.

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+var initPassport = require('./passport/init');
+var dbConfig = require('./db');
+mongoose.connect(dbConfig.url);
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
-var workouts = require('./routes/workouts');
+// var users = require('./routes/users');
+// var workouts = require('./routes/workouts');
 
 var tasks = require('./misc/tasks');
 
@@ -24,10 +32,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/workouts', workouts);
+// Initialize Passport
+initPassport(passport);
+
+app.use('/', routes(passport));
+// app.use('/users', users);
+// app.use('/workouts', workouts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
