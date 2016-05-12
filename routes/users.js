9f13a98ff1633aa237ec8f46d3a33bec0b358555
Carson Lourenco <carson.lourenco101@gmail.com>
2016-05-12 13:40:21 +1200

    error handling for invalid portal accounts

--- routes/users.js
+++ routes/users.js
@@ -53,58 +53,72 @@ router.get('/:id/workouts/:from/:to', function (req, res, next) {
   var userId = decodeURIComponent(req.params.id);
   var from = req.params.from; // In seconds for Skyrim
   var to = req.params.to; // In seconds for Skyrim
-  var sendData = builder.create('data').ele('workouts');
+  var sendData = builder.create('data');
 
-  ExerciseDotCom.find({
-    userEmail: userId,
-    dateRetrieved: {
-      $gt: new Date(parseInt(from) * 1000),
-      $lt: new Date(parseInt(to) * 1000)
-    }
-  }, function (err, workouts) {
-    if (err) {
-      res.send(err);
-      return;
-    }
-
-    for(var i in workouts) {
-      // Skyrim attributes, this will be generalised in the future so it can apply to more than one game.
-      var health = 0;
-      var stamina = 0;
-      var magicka = 0;
-
-      for (var j in workouts[i].data.workout_exercises) {
-        if (workouts[i].data.workout_exercises.hasOwnProperty(j)) {
-          var exerciseData = workouts[i].data.workout_exercises[j];
-          if (exerciseData.hasOwnProperty('total_reps') && exerciseData.total_reps > 0) {
-            // Exercise has reps, add points to health.
-            health += exerciseData.total_points;
-          }
-          else if (exerciseData.hasOwnProperty('distance') && typeof parseFloat(exerciseData['distance']) === 'number' && parseFloat(exerciseData.distance) > 0) {
-            // Exercise has distance, add points to stamina
-            stamina += exerciseData.total_points;
-          } else {
-            // Otherwise, add points to magicka
-            magicka += exerciseData.total_points;
-          }
+  User.find({
+    email: userId
+  }, function (err, users) {
+    if(users.length === 0){
+      sendData.ele({
+          errorCode: "404",
+          errorMessage: "User not found"
+      });
+      res.status(404).send(sendData.end({ pretty: true}));
+    } else {
+      ExerciseDotCom.find({
+        userEmail: userId,
+        dateRetrieved: {
+          $gt: new Date(parseInt(from) * 1000),
+          $lt: new Date(parseInt(to) * 1000)
         }
-      }
+      },
+      function (err, workouts) {
+        if (err) {
+          res.send(err);
+          return;
+        }
 
-      // Build XML document.
-      sendData.ele('workout').ele({
-        syncDate: Math.floor((new Date(Date.now())).valueOf() / 1000), // seconds
-        workoutDate: workouts[i].data.workout_date, //seconds
-        health: health,
-        stamina: stamina,
-        magicka: magicka
-      }).up();
-      workouts[i].used = true;
-      workouts[i].save();
+        sendData.ele('workouts');
+
+        for(var i in workouts) {
+          // Skyrim attributes, this will be generalised in the future so it can apply to more than one game.
+          var health = 0;
+          var stamina = 0;
+          var magicka = 0;
+
+          for (var j in workouts[i].data.workout_exercises) {
+            if (workouts[i].data.workout_exercises.hasOwnProperty(j)) {
+              var exerciseData = workouts[i].data.workout_exercises[j];
+              if (exerciseData.hasOwnProperty('total_reps') && exerciseData.total_reps > 0) {
+                // Exercise has reps, add points to health.
+                health += exerciseData.total_points;
+              }
+              else if (exerciseData.hasOwnProperty('distance') && typeof parseFloat(exerciseData['distance']) === 'number' && parseFloat(exerciseData.distance) > 0) {
+                // Exercise has distance, add points to stamina
+                stamina += exerciseData.total_points;
+              } else {
+                // Otherwise, add points to magicka
+                magicka += exerciseData.total_points;
+              }
+            }
+          }
+
+          // Build XML document.
+          sendData.ele('workout').ele({
+            syncDate: Math.floor((new Date(Date.now())).valueOf() / 1000), // seconds
+            workoutDate: workouts[i].data.workout_date, //seconds
+            health: health,
+            stamina: stamina,
+            magicka: magicka
+          }).up();
+          workouts[i].used = true;
+          workouts[i].save();
+        }
+        // Return XML.
+        res.send(sendData.end({pretty: true}));
+      });
     }
-
-    // Return XML.
-    res.send(sendData.end({pretty: true}));
-  });
+  })
 });
 
 module.exports = router;
