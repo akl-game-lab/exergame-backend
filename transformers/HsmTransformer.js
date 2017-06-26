01e94cb5a2175048c82bb00292ddcb53307c301e
Cameron McCarthy <cmcc544@UoA.auckland.ac.nz>
2017-06-26 14:02:55 +1200

    Entry name changed

--- transformers/HsmTransformer.js
+++ transformers/HsmTransformer.js
@@ -11,7 +11,7 @@ module.exports = class {
 			var health = 0;
 			var stamina = 0;
 			var magicka = 0;
-			String information = "";
+			var workoutInfo = "";
 
 			for (var j in workouts[i].data.workout_exercises) {
 				if (workouts[i].data.workout_exercises.hasOwnProperty(j)) {
@@ -19,21 +19,21 @@ module.exports = class {
 					if (exerciseData.hasOwnProperty('total_reps') && exerciseData.total_reps > 0) {
 						// Exercise has reps, add points to health.
 						health += exerciseData.total_points;
-						information += exerciseData.name;
-						information += ", Health + ";
-						information += String(exerciseData.total_points);
+						workoutInfo += exerciseData.name;
+						workoutInfo += ", Health + ";
+						workoutInfo += String(exerciseData.total_points);
 					} else if (exerciseData.hasOwnProperty('distance') && typeof parseFloat(exerciseData.distance) === 'number' && parseFloat(exerciseData.distance) > 0) {
 						// Exercise has distance, add points to stamina
 						stamina += exerciseData.total_points;
-						information += exerciseData.name;
-						information += ", Stamina + ";
-						information += String(exerciseData.total_points);
+						workoutInfo += exerciseData.name;
+						workoutInfo += ", Stamina + ";
+						workoutInfo += String(exerciseData.total_points);
 					} else {
 						// Otherwise, add points to magicka
 						magicka += exerciseData.total_points;
-						information += exerciseData.name;
-						information += ", Magicka + ";
-						information += String(exerciseData.total_points);
+						workoutInfo += exerciseData.name;
+						workoutInfo += ", Magicka + ";
+						workoutInfo += String(exerciseData.total_points);
 					}
 
 				}
@@ -48,7 +48,7 @@ module.exports = class {
 				magicka: magicka,
 				
 				//Assuming just add extra exercise.json information here, added a string to list exercise information
-				workoutData: information
+				workoutData: workoutInfo
 			});
 
 			workouts[i].used = true;
