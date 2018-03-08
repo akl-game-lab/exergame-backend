d375da41c17f267552cfd2eeaaa3c9724a490c08
hsco <henryscott56@gmail.com>
2018-03-08 15:18:32 +1300

    Add weekly data fetch for all users

--- misc/tasks.js
+++ misc/tasks.js
@@ -15,6 +15,12 @@ schedule.scheduleJob({ minute: [0, 20, 40] }, function () {
 	getUserData();
 });
 
+//Run Weekly at 10am on a Friday ([minute] [hour] [day of month] [month] [day of week] )
+schedule.scheduleJob('0 10 * * 5', function () {
+	log.info('Updating User data');
+	getAllUserData();
+});
+
 function getUserData() {
 	log.info('Attempting to find user data');
 	User.find({
@@ -30,6 +36,32 @@ function getUserData() {
 				// If user has exercise.com credentials, run casper.
 				if (users[i].credentials.exerciseDotCom.username && users[i].credentials.exerciseDotCom.plainPassword) {
 					log.debug('User found with credentials, retrieving exercise data');
+					retrieveExerciseData(users[i].email, users[i].credentials.exerciseDotCom.username, users[i].credentials.exerciseDotCom.plainPassword);
+				} else {
+					//temp
+					log.warn(`User: ${users[i].email} has no exercise.com credentials`);
+				}
+			}
+		}
+	});
+}
+
+function getAllUserData() {
+	log.info('Attempting to find user data for All Users');
+	User.find({
+		lastPlayed: {
+			'$gt': 0 // Has played ever.
+		}
+	},
+	function (err, users) {
+		if (err) {
+			log.error(err);
+		} else {
+			log.debug('Running weekly fetch of all user data');
+			for (var i = 0; i < users.length; i++) {
+				// If user has exercise.com credentials, run casper.
+				if (users[i].credentials.exerciseDotCom.username && users[i].credentials.exerciseDotCom.plainPassword) {
+					log.debug('User found with credentials, retrieving exercise data. All User fetch');
 					retrieveExerciseData(users[i].email, users[i].credentials.exerciseDotCom.username, users[i].credentials.exerciseDotCom.plainPassword);
 				} else {
 					//temp
